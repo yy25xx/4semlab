cd03e50888e52fe2db5b40e94432bf1c1d7f3c90
xx25yy <135425934+xx25yy@users.noreply.github.com>
2024-07-13 07:45:17 +0530

    Update mergesort.c

--- mergesort.c
+++ mergesort.c
@@ -2,29 +2,50 @@
 #include <stdlib.h>
 #include <time.h>
 
-int compare(const void *a, const void *b) {
-    return (*(int*)a - *(int*)b);
+void merge(int arr[], int l, int m, int r) {
+    int n1 = m - l + 1, n2 = r - m, L[n1], R[n2];
+    for (int i = 0; i < n1; i++) L[i] = arr[l + i];
+    for (int j = 0; j < n2; j++) R[j] = arr[m + 1 + j];
+    for (int i = 0, j = 0, k = l; k <= r; k++) {
+        if (i < n1 && (j >= n2 || L[i] <= R[j])) arr[k] = L[i++];
+        else arr[k] = R[j++];
+    }
+}
+
+void mergeSort(int arr[], int l, int r) {
+    if (l < r) {
+        int m = l + (r - l) / 2;
+        mergeSort(arr, l, m);
+        mergeSort(arr, m + 1, r);
+        merge(arr, l, m, r);
+    }
+}
+
+void printArray(int arr[], int size) {
+    for (int i = 0; i < size; i++) printf("%d ", arr[i]);
+    printf("\n");
 }
 
 int main() {
     int n;
     printf("****Merge Sort*****\nEnter the size of an array: ");
     scanf("%d", &n);
+
     int arr[n];
     srand(time(0));
     for (int i = 0; i < n; i++) arr[i] = rand() % n;
 
     printf("Before Sort: ");
-    for (int i = 0; i < n; i++) printf("%d ", arr[i]);
-    printf("\n");
+    printArray(arr, n);
 
     clock_t start = clock();
-    qsort(arr, n, sizeof(int), compare);
+    mergeSort(arr, 0, n - 1);
     clock_t end = clock();
 
     printf("After Sort: ");
-    for (int i = 0; i < n; i++) printf("%d ", arr[i]);
-    printf("\nTime taken: %f seconds\n", (double)(end - start) / CLOCKS_PER_SEC);
+    printArray(arr, n);
+
+    printf("Time taken: %f seconds\n", (double)(end - start) / CLOCKS_PER_SEC);
 
     return 0;
 }
